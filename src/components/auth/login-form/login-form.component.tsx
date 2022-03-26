2d53809a8be4125edf04fd9aaae5395acd88be62
Peixoto <nuno_peixoto1997@hotmail.com>
2022-03-26 14:55:13 +0000

    Add tailwindcss

--- src/components/auth/login-form/login-form.component.tsx
+++ src/components/auth/login-form/login-form.component.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const LoginForm: React.FC = () => {
     return(
-        <>
-        login form works
-        </>
+        <h1 className="text-3xl font-bold underline">
+        Tailwind works
+        </h1>
     )
 }
 
