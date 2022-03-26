8414982c6e51246d22e069dc527b0b8d477d506d
Peixoto <nuno_peixoto1997@hotmail.com>
2022-03-26 15:09:05 +0000

    Create login component skeleton

--- src/components/auth/login-form/login-form.component.tsx
+++ src/components/auth/login-form/login-form.component.tsx
@@ -1,10 +1,18 @@
+import { Button, TextField } from '@mui/material';
 import React from 'react';
 
 const LoginForm: React.FC = () => {
     return(
-        <h1 className="text-3xl font-bold underline">
-        Tailwind works
-        </h1>
+        <div className="flex justify-center items-center flex-col h-screen gap-8">
+            <h1 className="text-5xl"> Case Label </h1>
+            <div className="flex flex-col gap-2">
+                <TextField label="Emal" className="w-80" type="email" required />
+                <TextField label="Password" className="w-80" type="password" required />
+            </div>
+            <Button variant="contained" className="w-80">
+                <span className="p-1"> Login </span>
+            </Button>
+        </div>
     )
 }
 
