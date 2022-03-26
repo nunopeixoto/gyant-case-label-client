50384096c011bb2ebfd6e4861385ce41a2ada09e
Peixoto <nuno_peixoto1997@hotmail.com>
2022-03-26 15:48:04 +0000

    Add not empty validations to e-mail and password fields on sign up

--- src/components/auth/signup-form/signup-form.component.tsx
+++ src/components/auth/signup-form/signup-form.component.tsx
@@ -1,19 +1,54 @@
 import { Button, TextField, Link as MuiLink } from '@mui/material';
 import { Link } from "react-router-dom";
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignupForm: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [emailErrored, setEmailErrored] = useState(false);
+
+    const [password, setPassword] = useState('');
+    const [passwordErrored, setPasswordErrored] = useState(false);
+
+    const handleSignup = () => {
+        setEmailErrored(false);
+        if (!email) {
+            setEmailErrored(true);
+        }
+        
+        setPasswordErrored(false);
+        if (!password) {
+            setPasswordErrored(true);
+        }
+    }
     return(
         <div className="flex justify-center items-center flex-col h-screen gap-8">
             <h1 className="text-5xl"> Sign up </h1>
             <div className="flex flex-col gap-2">
-                <TextField label="Emal" className="w-80" type="email" required />
-                <TextField label="Password" className="w-80" type="password" required />
+                <TextField
+                    label="E-mail" 
+                    className="w-80" 
+                    type="email" 
+                    required 
+                    helperText={emailErrored && "Please enter a valid e-mail."}
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    error={emailErrored}
+                />
+                <TextField
+                    label="Password"
+                    className="w-80"
+                    type="password"
+                    required 
+                    helperText={passwordErrored && "Password can't be empty."}
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    error={passwordErrored}
+                />
                 <Link to="/login" className="justify-self-start self-start mt-2">
                     <MuiLink> Login </MuiLink>
                 </Link>
             </div>
-            <Button variant="contained" className="w-80">
+            <Button variant="contained" className="w-80" onClick={handleSignup}>
                 <span className="p-1"> Sign up </span>
             </Button>
         </div>
