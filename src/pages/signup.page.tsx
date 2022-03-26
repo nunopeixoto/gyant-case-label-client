a9b4dfacbad64e5d6bb3ff8c852ea01e9c3b6d00
Peixoto <nuno_peixoto1997@hotmail.com>
2022-03-26 15:37:15 +0000

    Rename signup component to signup-form for consistency

--- src/pages/signup.page.tsx
+++ src/pages/signup.page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SignupForm } from '../components/auth/signup/signup.component';
+import { SignupForm } from '../components/auth/signup-form/signup-form.component';
 
 const SignupPage: React.FC = () => {
     return <SignupForm></SignupForm>
