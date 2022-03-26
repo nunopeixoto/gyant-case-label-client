c9b23acf75b42ea65c8f7e67210b4f68c9a336e0
Peixoto <nuno_peixoto1997@hotmail.com>
2022-03-26 16:45:43 +0000

    Protect "/case" route (auth required)

---
+++ src/routes/protected-route.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navigate } from 'react-router';
+import { useAppSelector } from '../app/hooks'
+import { selectCurrentUser } from '../slices/auth.slice'
+
+const ProtectedRoute: React.FC<any> = ({ children }) => {
+    const user = useAppSelector((state) => selectCurrentUser(state));
+    return user ? children : <Navigate to="/login"/>
+}
+
+export { ProtectedRoute }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,12 +3,17 @@ import { Routes as ReactRouterRoutes, Route } from "react-router-dom";
 import { CasePage } from '../pages/case.page';
 import { LoginPage } from '../pages/login.page';
 import { SignupPage } from '../pages/signup.page';
+import { ProtectedRoute } from './protected-route.component';
 
 const Routes: React.FC = () => {
     return (<ReactRouterRoutes>
         <Route path="login" element={<LoginPage /> } />
         <Route path="signup" element={<SignupPage /> } />
-        <Route path="case" element={<CasePage /> } />
+        <Route path="case" element={
+            <ProtectedRoute>
+                <CasePage /> 
+            </ProtectedRoute>
+        } />
     </ReactRouterRoutes>)
 };
 
